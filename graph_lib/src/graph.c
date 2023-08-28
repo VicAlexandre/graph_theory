e7c1c51157f7c93b60633a8080e7ed9e59cc2a2e
Victor Alexandre <varm@ic.ufal.br>
2023-08-28 20:11:35 -0300

    refactor: :recycle: Implements graphs entirely with dynamic allocation

--- graph_lib/src/graph.c
+++ graph_lib/src/graph.c
@@ -1,8 +1,53 @@
 #include "graph.h"
 
+graph *create_graph()
+{
+    graph *g = malloc(sizeof(graph));
+    if (!g)
+        return NULL;
+
+    g->num_vertices = 1;
+    g->adj_list = malloc(sizeof(node *));
+    g->adj_list[0] = NULL;
+    return g;
+}
+
+void destroy_graph(graph *g)
+{
+    if (!g)
+        return;
+
+    for (int i = 0; i < g->num_vertices; i++)
+    {
+        node *current_v = g->adj_list[i];
+        while (current_v)
+        {
+            node *temp_v = current_v;
+            current_v = current_v->next;
+            free(temp_v);
+        }
+    }
+    free(g->adj_list);
+    free(g);
+}
+
+void add_vertex(graph *g)
+{
+    if (!g)
+        return;
+
+    g->adj_list = realloc(g->adj_list, sizeof(node *) * (g->num_vertices + 1));
+    if (!g->adj_list)
+        return;
+    g->adj_list[g->num_vertices++] = NULL;
+}
+
 void print_graph(graph *g)
 {
-    for (int i = 0; i < VERTICES_AMOUNT; i++)
+    if (!g)
+        return;
+
+    for (int i = 0; i < g->num_vertices; i++)
     {
         printf("%d  ->  ", i + 1);
         node *current_v = g->adj_list[i];
@@ -15,29 +60,22 @@ void print_graph(graph *g)
     }
 }
 
-void create_edge(graph *g, int vertex_a, int vertex_b)
+void add_arc(graph *g, int src_vertex, int destination_vertex)
 {
-    node *new_v = malloc(sizeof(node));
-    new_v->vertex = vertex_b;
-    new_v->next = NULL;
-    node *current_v = g->adj_list[vertex_a - 1];
+    if (!g || src_vertex > g->num_vertices || destination_vertex > g->num_vertices)
+        return;
 
-    if (current_v == NULL)
-    {
-        g->adj_list[vertex_a - 1] = new_v;
-    }
-    else
-    {
-        while (current_v->next != NULL)
-        {
-            current_v = current_v->next;
-        }
-        current_v->next = new_v;
-    }
+    node *new_v = malloc(sizeof(node));
+    if (!new_v)
+        return;
+
+    new_v->vertex = destination_vertex;
+    new_v->next = g->adj_list[src_vertex - 1];
+    g->adj_list[src_vertex - 1] = new_v;
 }
 
 void add_edge(graph *g, int vertex_a, int vertex_b)
 {
-    create_edge(g, vertex_a, vertex_b);
-    create_edge(g, vertex_b, vertex_a);
+    add_arc(g, vertex_a, vertex_b);
+    add_arc(g, vertex_b, vertex_a);
 }
--- graph_lib/src/graph.h
+++ graph_lib/src/graph.h
@@ -1,20 +1,61 @@
-#include <stdio.h>
-#include <stdlib.h>
-#define VERTICES_AMOUNT 10
+#ifndef GRAPH_H
+#define GRAPH_H
 
+/*
+ * Includes
+ */
+#include <stdlib.h>
+
+/*
+ * Structs
+ */
+
+/// @brief A node of the adjacency list.
 typedef struct
 {
   int vertex;
   struct node *next;
 } node;
 
+/// @brief The graph, represented as an adjacency list.
 typedef struct
 {
-  node *adj_list[VERTICES_AMOUNT];
+  int num_vertices;
+  node **adj_list;
 } graph;
 
+/*
+ * Functions
+ */
+
+/// @brief Creates a graph with one vertex.
+/// @return A pointer to the graph.
+extern graph *create_graph();
+
+/// @brief Deallocates the graph and it's nodes.
+/// @param g The graph.
+extern void destroy_graph(graph *g);
+
+/// @brief Receives a graph G and adds a new vertex to it.
+/// @param g The graph.
+extern void add_vertex(graph *g);
+
+/// @brief Receives a graph G and prints the adjacency list of each vertex.
+/// @param g The graph.
 extern void print_graph(graph *g);
 
-extern void create_edge(graph *g, int vertex_a, int vertex_b);
+/// @brief Receives a graph G, two vertices and adds an arc between them.
+/// Should be used in directed graphs.
+/// @param g The graph.
+/// @param source_vertex The starting vertex of the directed edge.
+/// @param destination_vertex The ending vertex of the directed edge.
+extern void add_arc(graph *g, int src_vertex, int destination_vertex);
 
+/// @brief Receives a graph G, two vertices and adds an edge between them.
+/// Should be used in undirected graphs.
+/// @param g The graph.
+/// @param vertex_a The first vertex.
+/// @param vertex_b The second vertex.
 extern void add_edge(graph *g, int vertex_a, int vertex_b);
+
+#endif
